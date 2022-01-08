ff88c255eb4237567f4fc418c4dd3acf69002112
Fabio Rodríguez <65049200+Pachec00@users.noreply.github.com>
2022-01-08 17:27:20 +0100

    Update console.py
    Nuevos comandos

--- console.py
+++ console.py
@@ -1,24 +1,55 @@
-#consola del servidor (archivo de control)
-#este archivo forma parte de la estructura del servidor
-#este archivo sirve para el manejo del servidor durante la ejecucion
-
 import sys
 import os
 
+
 #lista de comandos
+#estos comandos no se ejecutan a la vez que el archivo del servidor
+
 run = 'python main.py'
 logs = 'cat logs.txt'
-users = 'cat users.txt'
+help = 'help'
+clear = 'clear'
+maintenance = 'python maintenance.py'
+
+          
+commands = ['run', 'logs','help', 'clear', 'maintenance']
 
 print('Consola del servidor iniciada')
-
 while True:
     command = str(input())
 
     if command == 'run':
-        os.system(run)
-    elif command == 'logs':
-        os.system(logs)
-    elif command == 'users':
-        os.system(users)
         
+        print('')
+        os.system(run)
+        
+    elif command == 'logs':
+        
+        print('')
+        os.system(logs)
+        
+    elif command == 'help':
+        
+        for i in range(0, len(commands)):
+            print('')
+            print(commands[i])
+            print('')
+            
+    elif command == 'clear':
+        
+        print('')
+        os.system(clear)
+        print('')
+        
+    elif command == 'maintenance':
+        
+        print('')
+        #maintenance commands
+        print('-users --> Grafica del numero de usuarios')
+        print('')
+        
+        command = str(input())
+        
+        if command == 'users':
+            
+            from maintenance import num_users
